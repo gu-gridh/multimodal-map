7cd3708ceaa33756b0f2848481f700ef31ead8a1
Arild Matsson <arild.matsson@gu.se>
2023-02-15 16:30:29 +0100

    Show places from Diana

--- projects/rephotography/App.vue
+++ projects/rephotography/App.vue
@@ -10,6 +10,19 @@
           <NpolarLayer
             capabilitiesUrl="https://geodata.npolar.no/arcgis/rest/services/Basisdata/NP_Ortofoto_Svalbard_WMTS_25833/MapServer/WMTS/1.0.0/WMTSCapabilities.xml"
           />
+
+          <ol-vector-layer z-index="1">
+            <ol-source-vector
+              url="https://diana.dh.gu.se/api/rephotography/place/"
+              :format="geoJsonFormat"
+              ref="source"
+            />
+            <ol-style>
+              <ol-style-circle :radius="10">
+                <ol-style-fill color="rgb(232 157 59)"></ol-style-fill>
+              </ol-style-circle>
+            </ol-style>
+          </ol-vector-layer>
         </template>
       </MapComponent>
     </template>
@@ -17,7 +30,7 @@
 </template>
 
 <script lang="ts" setup>
-import { provide } from "vue";
+import { provide, inject } from "vue";
 import { project as config } from "./project.json";
 import MainLayout from "@/MainLayout.vue";
 import Search from "./Search.vue";
@@ -25,4 +38,7 @@ import MapComponent from "@/components/MapComponent.vue";
 import NpolarLayer from "./NpolarLayer.vue";
 
 provide("config", config);
+
+const format = inject("ol-format");
+const geoJsonFormat = new format.GeoJSON();
 </script>
