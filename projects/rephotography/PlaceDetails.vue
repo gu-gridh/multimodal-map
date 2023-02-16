fc1af08702008e24ae34fbcc4521d94bf0dafe07
Arild Matsson <arild.matsson@gu.se>
2023-02-16 19:26:59 +0100

    List images for selected place

--- projects/rephotography/PlaceDetails.vue
+++ projects/rephotography/PlaceDetails.vue
@@ -1,15 +1,38 @@
 <script lang="ts" setup>
-import { mapStore } from "@/stores/store";
+import { watchEffect, ref } from "vue";
 import { storeToRefs } from "pinia";
+import { mapStore } from "@/stores/store";
+import axios from "axios";
 
 const { selectedFeature } = storeToRefs(mapStore());
+
+const objects = ref();
+
+watchEffect(async () => {
+  if (selectedFeature.value) {
+    const response = await axios.get(
+      "https://diana.dh.gu.se/api/rephotography/image/?place=" +
+        selectedFeature.value.get("comment")
+    );
+    objects.value = response.data.results;
+  } else {
+    objects.value = [];
+  }
+});
 </script>
 
 <template>
   <div v-if="selectedFeature" class="col-start-6 col-span-1">
     <div class="bg-white rounded-lg shadow-lg mb-4">
       <div class="px-8 py-6">
-        {{ selectedFeature.getProperties() }}
+        <h3 class="font-bold">{{ selectedFeature.get("name") }}</h3>
+
+        <div v-for="object in objects" :key="object.uuid">
+          <img
+            :src="`${object.iiif_file}/full/180,/0/default.jpg`"
+            width="100"
+          />
+        </div>
       </div>
     </div>
   </div>
