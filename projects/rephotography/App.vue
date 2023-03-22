805cb410386c4aad19fdcc50f5f7131707b63232
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2023-03-22 08:31:28 +0100

    Cleaner code

--- projects/rephotography/App.vue
+++ projects/rephotography/App.vue
@@ -28,4 +28,20 @@ body {
   font-weight:100;
 }
 
+#app .left-pane{
+  background-color:grey;
+  width:700px;
+  padding:60px 250px 50px 60px;
+  background: linear-gradient(90deg, rgba(213,213,213,1) 0%, rgba(213,213,213,1) 30%, rgba(213,213,213,0.8) 50%,  rgba(213,213,213,0) 100%);
+}
+
+#app .detail-view{
+  height:100vh;
+  background-color:rgba(255,255,255,0.9);
+  width:100%;
+
+  transition: all 0.5s ease-in-out;
+  backdrop-filter: blur(7px);
+}
+
 </style>
