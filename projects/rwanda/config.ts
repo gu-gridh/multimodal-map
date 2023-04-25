fe76bf8264f00c9fd726044af0ab363389060635
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2023-04-25 19:18:55 +0200

    Update config.ts

--- projects/rwanda/config.ts
+++ projects/rwanda/config.ts
@@ -10,9 +10,9 @@ export default <RephotographyProject>{
   },
   moreinfo: "Learn more...",
   about:
-    "A transdisciplinary project based on collaborative research focusing  on the glacial environment in northwest Svalbard. The core of the project is using field based visualizations and research analysis.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   projection: "EPSG:3857",
-  center: [30.0636, -1.9567],
+  center: [30.0636, -1.9520],
   zoom: 15,
   timeRange: [1600, new Date().getFullYear()],
 };
