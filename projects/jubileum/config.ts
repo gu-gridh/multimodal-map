3447ddfa21fea7046037c591bfa427507c55a73e
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2023-05-03 22:26:36 +0200

    Update config.ts

--- projects/jubileum/config.ts
+++ projects/jubileum/config.ts
@@ -7,6 +7,6 @@ export default <JubileumProject>{
   about:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec nulla imperdiet, sollicitudin diam sit amet, porttitor urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec nulla imperdiet, sollicitudin diam sit amet, porttitor urna.",
   projection: "EPSG:3857",
-  center: [11.979583, 57.697167],
+  center: [11.983, 57.696],
   zoom: 17,
 };
