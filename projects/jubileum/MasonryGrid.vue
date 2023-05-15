08bb1ecef1f13139e8b406026c45a5367559baef
Tristan Bridge <tbridge360@gmail.com>
2023-05-15 09:26:33 +0200

    updated Masonry

--- projects/jubileum/MasonryGrid.vue
+++ projects/jubileum/MasonryGrid.vue
@@ -1,61 +1,78 @@
 <template>
-  <div class="masonry-grid">
-    <router-link
-      v-for="image in images"
-      :key="image.uuid"
-      :to="`/detail/image/${image.id}`"
-      class="grid-item"
-    >
-      <img :src="`${image.iiif_file}/full/400,/0/default.jpg`" />
-    </router-link>
-  </div>
+  <VueMasonryWall
+    :key="layoutKey"
+    class="masonry-wall"
+    :items="images"
+    :options="{ columnWidth: 250, gutter: 5, percentPosition: true }"
+  >
+    <template v-slot:default="{ item }">
+      <router-link
+        :key="item.uuid"
+        :to="`/detail/image/${item.id}`"
+        class="grid-item"
+      >
+        <img :src="`${item.iiif_file}/full/400,/0/default.jpg`" @load="imageLoaded" />
+      </router-link>
+    </template>
+  </VueMasonryWall>
 </template>
 
 <script lang="ts" setup>
-import { ref, inject, onMounted } from "vue";
+import { ref, inject, onMounted, defineComponent } from "vue";
 import type { Image } from "./types";
 import type { DianaClient } from "@/assets/diana";
+import VueMasonryWall from "@yeger/vue-masonry-wall";
 
 const diana = inject("diana") as DianaClient;
 
-const images = ref<Array<Image>>();
+const images = ref<Array<Image>>([]);
+
+let layoutKey = ref(0);
+let loadedImagesCount = ref(0);
+
+const imageLoaded = () => {
+  loadedImagesCount.value++;
+  if (loadedImagesCount.value === images.value?.length) {
+    refreshMasonry();
+  }
+};
+
+const refreshMasonry = () => {
+  layoutKey.value++;
+}
 
 onMounted(async () => {
   images.value = await diana.listAll<Image>("image");
 });
+
+defineComponent({
+  components: {
+    VueMasonryWall,
+  },
+});
 </script>
 
 <style>
-.masonry-grid {
+.masonry-wall {
   position: absolute;
   top: 0px;
-  width: 100%; /* Make the grid responsive */
-  height: 100%; /* Make the grid height responsive */
+  width: 100%; 
+  height: 100%; 
   z-index: 200;
   background-color: rgb(234, 228, 219);
-  padding: 0px 10px 10px 450px;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  grid-template-rows: masonry;
-  grid-gap: 5px;
+  padding: 0px 10px 10px 10px; 
   overflow-y: scroll;
 }
 
-.grid-item img {
-  max-width: 100%; /* Make the images responsive */
-  height: auto;
+@media (min-width: 768px) {
+  .masonry-wall {
+    padding: 0px 10px 10px 450px; 
+  }
 }
 
-.grid-item {
-  transition: all 0.2s ease-in-out;
-}
-
-.grid-item:hover {
+.masonry-item:hover {
   transform: scale(1.1);
+  z-index: 300; 
 }
 
-.grid-item img {
-  max-width: 100%; /* Make the images responsive */
-  height: auto;
-}
 </style>
