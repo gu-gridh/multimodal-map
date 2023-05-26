03deca05aa162542fa9893f38135f2dfa4485493
kikkipedia <kristinakerlund@gmail.com>
2023-05-26 11:43:02 +0200

    Added if-statement for capitalizing first letter-function

--- projects/rwanda/ObjectViewPlaceCard.vue
+++ projects/rwanda/ObjectViewPlaceCard.vue
@@ -9,9 +9,12 @@ defineProps<{
     }>();
 
     const capitalize = (word: String) => {
-      const first = word[0].toUpperCase()
-      const rest = word.slice(1)
-      return first + rest
+      if (word !== '') {
+        const first = word[0].toUpperCase()
+        const rest = word.slice(1)
+        return first + rest
+      }
+      else return word
     }
 
     const center = ref([30.0636, -1.9520]);
@@ -20,9 +23,6 @@ defineProps<{
     const rotation = ref(0);
     const strokeWidth = ref(5);
     const strokeColor = ref("red");
-
-    //check if data is missing - TODO
-
 
 </script>
 
@@ -51,49 +51,48 @@ defineProps<{
               <ol-source-osm />
             </ol-tile-layer>
             <ol-vector-layer>
-            <ol-source-vector>
-              <ol-feature>
-                <ol-geom-line-string
-                  :coordinates="place.geometry.coordinates[0]"
-                ></ol-geom-line-string>
-                <ol-style>
-                  <ol-style-stroke
-                    :color="strokeColor"
-                    :width="strokeWidth"
-                  ></ol-style-stroke>
-                </ol-style>
-              </ol-feature>
-            </ol-source-vector>
-          </ol-vector-layer>
+              <ol-source-vector>
+                <ol-feature>
+                  <ol-geom-line-string
+                    :coordinates="place.geometry.coordinates[0]"
+                  ></ol-geom-line-string>
+                  <ol-style>
+                    <ol-style-stroke
+                      :color="strokeColor"
+                      :width="strokeWidth"
+                    ></ol-style-stroke>
+                  </ol-style>
+                </ol-feature>
+              </ol-source-vector>
+            </ol-vector-layer>
           </ol-map>
         </div>
         <!-- meta-data -->
         <div class="metadata-content"> 
-      <h1>{{ capitalize(place.properties.type.text) }}</h1>
-      <div class="meta-item">{{ capitalize(place.properties.description) }}</div>
-      <div class="meta-item"> {{place.properties.is_existing ? 'Existing' : 'Non-existing'}} - {{ place.properties.is_iconic ? 'Iconic' : 'Not iconic'}} - {{ place.properties.is_private ? 'Private' : 'Public'}}</div>
-      <div class="meta-item" v-if="place.properties.parent_place !== null">
-        <a :href="place.properties.parent_place.id">
-        <div class="category-button">
-          Parent place
-        </div>
-      </a>
-    </div>
+          <h1>{{ capitalize(place.properties.type.text) }}</h1>
+          <div class="meta-item">{{ capitalize(place.properties.description) }}</div>
+          <div class="meta-item"> {{place.properties.is_existing ? 'Existing' : 'Non-existing'}} - {{ place.properties.is_iconic ? 'Iconic' : 'Not iconic'}} - {{ place.properties.is_private ? 'Private' : 'Public'}}</div>
+          <div class="meta-item" v-if="place.properties.parent_place !== null">
+            <a :href="place.properties.parent_place.id">
+              <div class="category-button">
+                Parent place
+              </div>
+            </a>
+          </div>
        
-    <div style="margin-top:30px;">
-        <div  v-for="name in place.properties.names">
-        <div  v-if="name !== null || undefined">
-        
-          <div class="meta-item"  v-if="name.languages && name.languages.length > 0"><div class="lang">{{ name.languages[0].abbreviation }}</div> <div class="long-name" style="font-weight:600;" v-if="name.text">{{ name.text }}</div></div>      
-    
-        <div style="width:100%; float:left; margin-bottom:30px; padding-left:45px;">
-          <div class="meta-item" v-if="name.period !== null">Period: {{ name.period.start_year }} - {{ name.period.end_year }}. {{ capitalize(name.period.text) }}</div>
-              <div class="meta-item" v-if="name.informants && name.informants.length > 0">Informant: <span v-for="informant in name.informants">{{ name.informants[0].custom_id }}. {{ name.informants[0].note }}</span></div>
+          <div style="margin-top:30px;">
+            <div  v-for="name in place.properties.names">
+              <div  v-if="name !== null || undefined">
+                <div class="meta-item"  v-if="name.languages && name.languages.length > 0"><div class="lang">{{ name.languages[0].abbreviation }}</div> <div class="long-name" style="font-weight:600;" v-if="name.text">{{ name.text }}</div>
+              </div>      
+              <div style="width:100%; float:left; margin-bottom:30px; padding-left:45px;">
+                <div class="meta-item" v-if="name.period !== null">Period: {{ name.period.start_year }} - {{ name.period.end_year }}. {{ capitalize(name.period.text) }}</div>
+                <div class="meta-item" v-if="name.informants && name.informants.length > 0">Informant: <span v-for="informant in name.informants">{{ name.informants[0].custom_id }}. {{ name.informants[0].note }}</span></div>
                 <div class="meta-item" v-if="name.referent">Comment: {{ name.referent.comment }}</div>
-                  <div class="meta-item" v-if="name.note">Note: {{ name.note }}</div>
+                <div class="meta-item" v-if="name.note">Note: {{ name.note }}</div>
+            </div>
+          </div>  
         </div>
-      </div>  
-      </div>
       </div>
     </div>
     </div>
