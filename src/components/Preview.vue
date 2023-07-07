600068306c0331ec25ea14da992ad040144f7bc6
kikkipedia <49567322+kikkipedia@users.noreply.github.com>
2023-07-07 14:45:38 +0200

    Component for previewing content in multimodial projects

---
+++ src/components/Preview.vue
@@ -0,0 +1,37 @@
+<!-- Component for previewing content -->
+<script setup lang="ts">
+import { watchEffect, ref, inject } from "vue";
+import { storeToRefs } from "pinia";
+import { mapStore } from "@/stores/store";
+import type { DianaClient } from "@/assets/diana";
+
+//when a place on map is clicked
+const { selectedFeature } = storeToRefs(mapStore())
+
+watchEffect(async () => {
+  if (selectedFeature.value) {
+    const place_of_interest = selectedFeature.value.getId();
+  }
+});
+
+function deselectPlace() {
+  selectedFeature.value = undefined;
+}
+const store = mapStore();
+const {center, zoom} = storeToRefs(store);
+
+</script>
+
+<template>
+    <div v-if="selectedFeature" class="mapview-preview">
+ 
+        <div class="px-2 py-6">
+        </div>
+    </div>
+</template>
+
+<style>
+    .mapview-preview-container{
+    height:calc(100vh - 80px);
+    }
+</style>
