01a68b8675509c95a259b2526ee1a021fc24f2b9
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2023-09-12 22:14:17 +0200

    Slightly better balance

--- projects/etruscantombs/MapViewPreview.vue
+++ projects/etruscantombs/MapViewPreview.vue
@@ -100,11 +100,11 @@ function deselectPlace() {
   flex-direction: column;
   align-items: left;
   width: auto;
-  height: calc(100vh - 210px);
+  height: calc(100vh - 220px);
   pointer-events: auto !important;
   border-radius: 15px;
   margin-right: 30px;
-  margin-top: 60px;
+  margin-top: 70px;
   padding: 0px;
   margin-bottom: 20px;
   overflow: hidden;
