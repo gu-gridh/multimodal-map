2c0b67a1b392538db9f28807d12f2951efac07d5
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2023-09-21 15:11:45 +0200

    placecard styling

--- projects/etruscantombs/PlaceViewCard.vue
+++ projects/etruscantombs/PlaceViewCard.vue
@@ -3,108 +3,98 @@ import router from './router'
 import { ref, inject, onMounted } from "vue"
 import markerIcon from "@/assets/marker-red.svg";
 
-    const props = defineProps<{
-        id: number;
-    }>();
+const props = defineProps<{
+  id: number;
+}>();
 
-    const capitalize = (word: String) => {
-      if (word !== '') {
-        const first = word[0].toUpperCase()
-        const rest = word.slice(1)
-        return first + rest
-      }
-      else return word
+const capitalize = (word: String) => {
+  if (word !== '') {
+    const first = word[0].toUpperCase()
+    const rest = word.slice(1)
+    return first + rest
+  }
+  else return word
+}
+
+const title = ref<string | null>(null);
+const necropolisName = ref<string | null>(null);
+const chambers = ref<number | null>(null);
+const type = ref<string | null>(null);
+const period = ref<string | null>(null);
+const subtitle = ref<string | null>(null);
+const description = ref<string | null>(null);
+
+const projection = ref("EPSG:4326");
+const zoom = ref(15);
+const rotation = ref(0);
+const strokeWidth = ref(5);
+const strokeColor = ref("red");
+const center = ref([11.999722, 42.224444])
+const minZoom = ref(12)
+
+const format = inject("ol-format");
+const geoJson = new format.GeoJSON();
+
+const url = "https://diana.dh.gu.se/api/etruscantombs/geojson/place/" + props.id
+
+const setCenter = async () => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
+    const placeData = await response.json();
+    if (placeData && placeData.geometry && placeData.geometry.coordinates) {
+      center.value = placeData.geometry.coordinates;
+    }
+  } catch (error) {
+    console.error("Error fetching place data:", error);
+  }
+};
 
-    const title = ref<string | null>(null);  
-    const necropolisName = ref<string | null>(null);  
-    const chambers = ref<number | null>(null);
-    const type = ref<string | null>(null);
-    const period = ref<string | null>(null);
-    const subtitle = ref<string | null>(null);
-    const description = ref<string | null>(null);  
+const fetchPlaceData = async () => {
+  try {
+    const response = await fetch(`https://diana.dh.gu.se/api/etruscantombs/geojson/place/?id=${props.id}`);
+    if (response.ok) {
+      const data = await response.json();
+      const feature = data.features[0];
+      const properties = feature.properties;
 
-    const projection = ref("EPSG:4326");
-    const zoom = ref(15);
-    const rotation = ref(0);
-    const strokeWidth = ref(5);
-    const strokeColor = ref("red");
-    const center = ref([11.999722, 42.224444])
-    const minZoom = ref(12)
+      // Assign fetched data to Vue Ref variables
+      title.value = properties.name || null;
+      necropolisName.value = properties.necropolis.text || null;
+      chambers.value = properties.number_of_chambers || null;
+      type.value = properties.type.text || null;
+      period.value = properties.epoch.text || null;
+      subtitle.value = properties.subtitle || null;
+      description.value = properties.description || null;
+    }
+  } catch (error) {
+    console.error("Failed to fetch data", error);
+  }
+};
 
-    const format = inject("ol-format");
-    const geoJson = new format.GeoJSON();
+// Center the map based on fetched data.
+onMounted(() => {
+  setCenter();
+  fetchPlaceData();  // Fetch image and details when component is mounted.
+});
 
-    const url = "https://diana.dh.gu.se/api/etruscantombs/geojson/place/"+ props.id
-
-      const setCenter = async () => {
-        try {
-          const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const placeData = await response.json();
-        if (placeData && placeData.geometry && placeData.geometry.coordinates) {
-          center.value = placeData.geometry.coordinates;
-          }
-      } catch (error) {
-        console.error("Error fetching place data:", error);
-      }
-    };
-
-    const fetchPlaceData = async () => {
-        try {
-          const response = await fetch(`https://diana.dh.gu.se/api/etruscantombs/geojson/place/?id=${props.id}`);
-          if (response.ok) {
-            const data = await response.json();
-            const feature = data.features[0];
-            const properties = feature.properties;
-            
-            // Assign fetched data to Vue Ref variables
-            title.value = properties.name || null;
-            necropolisName.value = properties.necropolis.text || null;
-            chambers.value = properties.number_of_chambers || null;
-            type.value = properties.type.text || null;
-            period.value = properties.epoch.text || null;
-            subtitle.value = properties.subtitle || null;
-            description.value = properties.description || null;
-          }
-      } catch (error) {
-        console.error("Failed to fetch data", error);
-      }
-  };
-
-  // Center the map based on fetched data.
-  onMounted(() => {
-    setCenter();
-    fetchPlaceData();  // Fetch image and details when component is mounted.
-  });
-   
 </script>
 
 <template>
-   <button class="place-back-button" @click="router.push({path: '/'})"></button>
+  <button class="place-back-button" @click="router.push({ path: '/' })"></button>
   <div class="place-meta-container">
-   
-      <div class="placecard-full">
-   
-        <div class="placecard-full-content">
-        
-          <!-- mini map -->
+
+    <div class="placecard-full">
+
+      <div class="placecard-full-content">
+
+        <!-- mini map -->
         <div class="mini-map">
-          <ol-map 
-            :loadTilesWhileAnimating="true"
-            :loadTilesWhileInteracting="true"
-            style="height:300px"
-          >
-            <ol-view
-              ref="view"
-              :center="center"
-              :rotation="rotation"
-              :zoom="zoom"
-              :projection="projection"
-              :minZoom="minZoom"
-            />
+          <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height:300px">
+            <ol-view ref="view" :center="center" :rotation="rotation" :zoom="zoom" :projection="projection"
+              :minZoom="minZoom" />
             <ol-tile-layer>
               <ol-source-osm />
             </ol-tile-layer>
@@ -113,19 +103,16 @@ import markerIcon from "@/assets/marker-red.svg";
               <ol-source-vector :url="url" :format="geoJson">
               </ol-source-vector>
               <ol-style>
-                <ol-style-icon 
-                :src="markerIcon" 
-                :scale="1.8" 
-                :displacement="[-10, 45]"
-                :anchor="[0.0, 0.0]"></ol-style-icon>
+                <ol-style-icon :src="markerIcon" :scale="1.8" :displacement="[-10, 45]"
+                  :anchor="[0.0, 0.0]"></ol-style-icon>
               </ol-style>
             </ol-vector-layer>
           </ol-map>
         </div>
         <div class="placecard-text">
-          <div class="placecard-title theme-color-text">Tomb {{ title }}</div> 
-          <div class="placecard-subtitle theme-color-text">{{ subtitle }}</div>       
-  
+          <div class="placecard-title theme-color-text">Tomb {{ title }}</div>
+          <div class="placecard-subtitle theme-color-text">{{ subtitle }}</div>
+
           <div class="placecard-metadata-content">
             <div class="metadata-item">
               <div class="label">Necropolis:</div>
@@ -143,12 +130,15 @@ import markerIcon from "@/assets/marker-red.svg";
               <div class="label">Period:</div>
               <div class="tag theme-color-text">{{ period }}</div>
             </div>
-          </div>  
-            <div class="placecard-metadata-content" style="margin-top:10px;" v-html="description"></div>
+          </div>
+          <div class="placecard-metadata-content" style="margin-top:10px;">
+            <div class="placecard-metadata-description" v-html="description">
+            </div>
+          </div>
         </div>
-    </div>
       </div>
-  
+    </div>
+
   </div>
 </template>
 
@@ -169,28 +159,26 @@ import markerIcon from "@/assets/marker-red.svg";
   position: absolute;
 }
 
-.place-meta-container{
+.place-meta-container {
   overflow-y: auto;
-  height:calc(100vh - 80px);
-  padding-bottom:30px;
-  padding-left:45px;
-  padding-right:20px;
+  height: calc(100vh - 80px);
+  padding-bottom: 30px;
+  padding-left: 45px;
+  padding-right: 20px;
 }
 
-#app .place-meta-container {
-
-}
+#app .place-meta-container {}
 
 .placecard-full {
-  margin-top:80px;
-  margin-left:50px;
-  color:black;
+  margin-top: 80px;
+  margin-left: 50px;
+  color: black;
   background-color: white;
   padding: 0px;
   box-shadow: -3px 0px 25px rgba(0, 0, 0, 0.3);
-  height:calc(100vh - 230px);
-  overflow:hidden;
-  overflow-y:auto;
+  height: calc(100vh - 230px);
+  overflow: hidden;
+  overflow-y: auto;
   margin-bottom: 10px;
   border-radius: 10px !important;
   -ms-overflow-style: none;
@@ -199,94 +187,98 @@ import markerIcon from "@/assets/marker-red.svg";
 }
 
 .placecard-full::-webkit-scrollbar {
-    width: 0 !important
-    }
+  width: 0 !important
+}
 
 .placecard-full-content {
-height:auto;
-width:100%;
-padding: 0px 0px 10px 0px;
+  height: auto;
+  width: 100%;
+  padding: 0px 0px 10px 0px;
 }
 
-.placecard-full-content h1{
-font-size:2.5em;
-color:rgb(180,100,100);
-margin-bottom:5px;
+.placecard-full-content h1 {
+  font-size: 2.5em;
+  color: rgb(180, 100, 100);
+  margin-bottom: 5px;
 }
 
-.placecard-full .category-button{
-  width:110px!important;
-padding:4px 18px!important;
-margin-top:15px;
-margin-bottom:20px;
+.placecard-full .category-button {
+  width: 110px !important;
+  padding: 4px 18px !important;
+  margin-top: 15px;
+  margin-bottom: 20px;
 
 }
 
-.mini-map{
-  width:100%;
-  height:35vh;
-  max-height:300px;
-  overflow:hidden;
-  background-color:grey;
-  margin-bottom:0px;
+.placecard-full .placecard-text{
+  padding:10px 30px 0px 30px;
+}
+
+.mini-map {
+  width: 100%;
+  height: 35vh;
+  max-height: 300px;
+  overflow: hidden;
+  background-color: grey;
+  margin-bottom: 0px;
 }
 
 
 /* For big screens */
 @media screen and (min-width: 1900px) {
-  .placecard-full .category-button{
-  width:125px!important;
-padding:5px 18px!important;
-margin-top:15px;
-}
+  .placecard-full .category-button {
+    width: 125px !important;
+    padding: 5px 18px !important;
+    margin-top: 15px;
+  }
 }
 
 
 /* For small screens */
 @media screen and (max-width: 900px) {
 
-  #app .place-meta-container{
-position:relative !important;
-margin-top:0px  !important;
-top:20px!important;
-height:auto;
-font-size:100%;
-width:100%;
-z-index:100;
-padding-right:20px !important;
-padding-left:20px !important;
-padding-bottom:0px !important;
-overflow-y:auto;
-}
+  #app .place-meta-container {
+    position: relative !important;
+    margin-top: 0px !important;
+    top: 20px !important;
+    height: auto;
+    font-size: 100%;
+    width: 100%;
+    z-index: 100;
+    padding-right: 20px !important;
+    padding-left: 20px !important;
+    padding-bottom: 0px !important;
+    overflow-y: auto;
+  }
 
   .place-back-button {
     left: 30px;
-  top: 70px;
-  width: 50px;
-  height: 50px;
-  z-index:1000;
-}
+    top: 70px;
+    width: 50px;
+    height: 50px;
+    z-index: 1000;
+  }
 
-.placecard-full {
-margin-top:30px;
-color:black;
-margin-left:0px;
-background-color: white;
-padding-bottom: 30px;
-box-shadow: 0px -15px 15px rgba(0, 0, 0, 0.1);
-transition: all 0.0s ease-in-out;
-height:auto;
-overflow:hidden;
-border-radius: 20px 20px 0px 0px !important;
-}
+  .placecard-full {
+    margin-top: 30px;
+    color: black;
+    margin-left: 0px;
+    background-color: white;
+    padding-bottom: 30px;
+    box-shadow: 0px -15px 15px rgba(0, 0, 0, 0.1);
+    transition: all 0.0s ease-in-out;
+    height: auto;
+    overflow: hidden;
+    border-radius: 20px 20px 0px 0px !important;
+  }
 
-.placecard-full .category-button{
-width:120px!important;
-padding:8px 15px!important;
-margin-top:15px;
-padding:0px;
-padding-left:0px;
-margin-bottom:0px!important;
-}
+  .placecard-full .category-button {
+    width: 120px !important;
+    padding: 8px 15px !important;
+    margin-top: 15px;
+    padding: 0px;
+    padding-left: 0px;
+    margin-bottom: 0px !important;
+  }
 }
 </style>
