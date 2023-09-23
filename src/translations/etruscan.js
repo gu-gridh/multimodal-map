9f89c884cacd7a427ca8e8583133111c87178c09
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2023-09-23 20:22:38 +0200

    Update etruscan.js

--- src/translations/etruscan.js
+++ src/translations/etruscan.js
@@ -7,14 +7,14 @@ const messages = {
       documentation: "Documentation by category",
       period: "Time period",
       tomb: "Type of tomb",
-      map: "mappa",
-      gallery: "galleria",
+      map: "map",
+      gallery: "gallery",
       infobutton: "More Info",
       languagebutton: "Italiano",
   
     },
     it: {
-      etruscantitle: "Tombe etrusche a camera",
+      etruscantitle: "Tombe Etrusche A Camera",
       etruscanabout: "Il portale è pubblicato da GRIDH presso l'Università di Goteborg con la collaborazione dell'Istituto Svedese di Studi Classici a Roma.",
       documentation: "Documentazione per Categoria",
       period: "Periodo di tempo",
