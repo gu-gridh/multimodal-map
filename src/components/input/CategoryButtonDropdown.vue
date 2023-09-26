61494678a9f3b83f151abe9695d4f94872601a54
Tristan Bridge <tbridge360@gmail.com>
2023-09-26 13:20:10 +0200

    styling for dropdown

--- src/components/input/CategoryButtonDropdown.vue
+++ src/components/input/CategoryButtonDropdown.vue
@@ -8,7 +8,7 @@
       @toggle="toggle(key)"
     />
   </div>
-  <select v-else :value="modelValue[0]" @change="dropdownToggle($event.target.value)">
+  <select v-else :value="modelValue[0]" class="dropdown" @change="dropdownToggle($event.target.value)">
     <option value="all">all</option>
     <option 
       v-for="(label, key) in categories"
@@ -62,4 +62,27 @@ function handleToggle(key: string) {
 </script>
 
 <style scoped>
+.dropdown {
+    color: white;
+    border-radius: 5px;
+    background-color: rgb(180, 100, 100);
+    padding: 3px 10px;
+    padding-right: 30px;
+    width: auto;
+    height: 32px;
+    background-image: url("/dropdown-arrow.png");
+    background-size: 15px;
+    background-repeat: no-repeat;
+    background-position: calc(100% - 10px) 10px;
+    -webkit-appearance: none;
+}
+
+.dropdown:hover {
+    background-color: rgb(160, 80, 80);
+}
+
+.dropdown:focus {
+    outline: none;
+    outline-color: rgb(140, 60, 60) !important;
+}
 </style>
