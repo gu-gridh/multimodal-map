53961deb34543f94506d61e55a931c8a4dfe1736
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2023-10-13 15:27:48 +0200

    Better centering of map

--- projects/rwanda/config.ts
+++ projects/rwanda/config.ts
@@ -12,7 +12,7 @@ export default <RwandaProject>{
   about:
     "Renaming and transformative processes in urban Rwanda",
   projection: "EPSG:3857",
-  center: [30.0636, -1.9520],
+  center: [30.055, -1.9520],
   zoom: 15,
   timeRange: [1600, new Date().getFullYear()],
 };
