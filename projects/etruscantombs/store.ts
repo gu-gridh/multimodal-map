c92425f6ab5e1e09a0a6e50d6873bd2060dfb425
Tristan Bridge <tbridge360@gmail.com>
2023-10-16 15:18:24 +0200

    map move based on necropolis

--- projects/etruscantombs/store.ts
+++ projects/etruscantombs/store.ts
@@ -10,6 +10,7 @@ export const etruscanStore = defineStore("etruscan", () => {
   const tagsLayerVisible = ref(false); // Default visibility state for tags layer
   const placesLayerVisible = ref(true); // Default visibility state for places layer
   const dataParams = ref<Record<string, string | number | null>>({});
+  const selectedNecropolisCoordinates = ref<[number, number] | null>(null);
 
-  return { categories, tags, tagsLayerVisible, placesLayerVisible, necropoli, tombType,  dataParams};
+  return { categories, tags, tagsLayerVisible, placesLayerVisible, necropoli, tombType,  dataParams, selectedNecropolisCoordinates };
 });
