0df33e78f9fd6301163f9f3be1e6900318472dc8
TristanBridge <tbridge360@gmail.com>
2023-11-23 15:36:34 +0100

    download data as json

--- projects/etruscantombs/Footer.vue
+++ projects/etruscantombs/Footer.vue
@@ -1,8 +1,11 @@
 <template>
   <footer id="footer">
     <div id="developer-info">
-      <a href="https://github.com/gu-gridh/etruscantombs#etruscan-tombs" target="_blank">GitHub repository and development</a> <br>
-      <a href="https://github.com/gu-gridh/etruscantombs#loading-data" target="_blank">Database and API documentation</a> <br>
+      <div class="links-stack border-style">
+        <a href="https://github.com/gu-gridh/etruscantombs#etruscan-tombs" target="_blank">GitHub repository and development</a>
+        <a href="https://github.com/gu-gridh/etruscantombs#loading-data" target="_blank">Database and API documentation</a>
+      </div>
+      <a href="#" class="download-link border-style" @click="downloadData">Download the data</a>
     </div>
     <a href="https://dh.gu.se/">
       <div id="PartnerLogo">+ ISVROMA</div>
@@ -14,6 +17,41 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const downloadData = async () => {
+  let url = 'https://diana.dh.gu.se/api/etruscantombs/geojson/place/';
+  let pageNumber = 0;
+
+  try {
+    while (url) {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const pageData = await response.json();
+      pageNumber++;
+
+      triggerDownload(pageData.features, `EtruscanTombsData_${pageNumber}.json`);
+
+      url = pageData.next ? pageData.next.replace(/^http:/, 'https:') : null;
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+const triggerDownload = (data: any[], filename: string) => {
+  const jsonBlob = new Blob([JSON.stringify(data)], { type: 'application/json' });
+  const downloadUrl = window.URL.createObjectURL(jsonBlob);
+  const link = document.createElement('a');
+  link.href = downloadUrl;
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  window.URL.revokeObjectURL(downloadUrl);
+};
 </script>
 
 <style>
@@ -66,16 +104,29 @@ a {
 }
 
 #developer-info {
-  float: left;
-  margin-left:50px;
-  margin-top:15px;
-padding:0px 0px 0px 15px;
- font-size:1.5em;
- line-height:1.4;
- font-weight:200;
- border-width: 0 0 0 0.5px;
- border-color:white;
- border-style:dashed;
+  display: flex; 
+  align-items: start; 
+  margin-left: 50px;
+  margin-top: 15px;
+  padding: 0 0 0 15px;
+  font-size: 1.5em;
+  line-height: 1.4;
+  font-weight: 200;
+}
+
+.border-style {
+  border-width: 0 0 0 0.5px;
+  border-color: white;
+  border-style: dashed;
+  padding-left: 10px; 
+}
+
+.links-stack {
+  margin-right: 20px; 
+}
+
+.links-stack a {
+  display: block;
 }
 
 #developer-info a:link{
