d8049bb0a84d0fc0e05d5ff88f472e4b1ea1a1be
TristanBridge <tbridge360@gmail.com>
2023-11-29 11:38:34 +0100

    jubileum tracking

--- projects/jubileum/main.ts
+++ projects/jubileum/main.ts
@@ -5,8 +5,17 @@ import router from "./router";
 import OpenLayersMap from "vue3-openlayers";
 import "vue3-openlayers/dist/vue3-openlayers.css";
 import "@/assets/main.css";
+import VueMatomo from 'vue-matomo'
 
 createApp(App) //
+  .use(VueMatomo, {
+    host: import.meta.env.VITE_ETRUSCAN_MATOMO_URL,
+    siteId: import.meta.env.VITE_ETRUSCAN_MATOMO_ID,
+    router: router,
+    enableLinkTracking: true,
+    trackInitialView: true,
+    debug: false
+  })
   .use(createPinia())
   .use(router)
   .use(OpenLayersMap)
