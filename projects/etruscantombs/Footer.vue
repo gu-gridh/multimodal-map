8416d3e5165c906912ac0e8fe42eb74fbe89f0cc
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2023-12-02 18:58:24 +0100

    Datset documentation link

--- projects/etruscantombs/Footer.vue
+++ projects/etruscantombs/Footer.vue
@@ -5,7 +5,10 @@
         <a class="site-link link" href="https://github.com/gu-gridh/etruscantombs#etruscan-tombs" target="_blank">GitHub repository and development</a>
         <a class="site-link link" href="https://github.com/gu-gridh/etruscantombs#database-and-api-documentation" target="_blank">Database and API documentation</a>
       </div>
+      <div class="links-stack">
+      <a class="site-link link" href="https://github.com/gu-gridh/etruscantombs#datasets" target="_blank">Dataset documentation</a>
       <a href="#" class="download-link link" @click="downloadData">Download the structured data</a>
+    </div>
     </div>
     <div class="partners">
     <a href="https://dh.gu.se/">
