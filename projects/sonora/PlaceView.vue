b36db6c41d5f974e921e4bb0813a731c9b0ceffd
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2024-01-23 16:09:01 +0100

    hidden scroll and blurry map in placeview

--- projects/sonora/PlaceView.vue
+++ projects/sonora/PlaceView.vue
@@ -2,35 +2,37 @@
 import { ref, defineProps, onMounted, inject, computed } from 'vue';
 import type { Image, Observation, Document, Pointcloud, Mesh } from './types';
 import type { DianaClient } from "@/assets/diana";
-import documentIcon from '@/assets/document.svg'; 
-import PlaceViewCard from "./PlaceViewCard.vue"; 
+import documentIcon from '@/assets/document.svg';
+import PlaceViewCard from "./PlaceViewCard.vue";
+import MapComponent from "@/components/MapComponent.vue";
 import { watch } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 const sort = ref('type');
-const groupedByYear = ref<{ [year: string]: (Image | Observation | Document | Pointcloud | Mesh )[] }>({});
+const groupedByYear = ref<{ [year: string]: (Image | Observation | Document | Pointcloud | Mesh)[] }>({});
 const { id } = defineProps<{ id: string; }>();
 let documents = ref<Document[]>([]); // Initialized as an empty array
 
 watch(() => route.params.id, async (newId) => {
   if (newId) {
     try {
-    const response = await fetch(`https://orgeldatabas.gu.se/webgoart/goart/organ.php?id=${newId}&lang=sv`);
-    if (!response.ok) {
+      const response = await fetch(`https://orgeldatabas.gu.se/webgoart/goart/organ.php?id=${newId}&lang=sv`);
+      if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
+      }
+      const data = await response.json();
 
-        documents.value = [];
-        for (let i = 0; i < data.no_docs; i++) {
+      documents.value = [];
+      for (let i = 0; i < data.no_docs; i++) {
         if (data[i.toString()]) {
-            documents.value.push(data[i.toString()]);
+          documents.value.push(data[i.toString()]);
         }
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }  }
+  }
 }, { immediate: true });
 
 onMounted(async () => {
@@ -72,61 +74,75 @@ onMounted(async () => {
 </script>
     
 <template>
-    <div class="main-container">
-        <div class="place-card-container">
-            <PlaceViewCard :id="id" />
-        </div>
-        <!-- Here we will show info of the place -->
-        <div class="place-view">
-            <div class="place-gallery-container">
-                <!-- Gallery of objects will show here with the ability to sort by TYPE of object -->
-                <!-- <p>ID: {{ id }}</p> -->
-                <!-- <p>PLACE: {{ place.id }}</p> -->
-                <div>
-                    <!-- <select v-model="sort" class="dropdown">
+  <div class="main-container">
+    <div class="place-card-container">
+      <PlaceViewCard :id="id" />
+    </div>
+    <!-- Here we will show info of the place -->
+    <div class="place-view">
+      <div class="place-gallery-container">
+        <!-- Gallery of objects will show here with the ability to sort by TYPE of object -->
+        <!-- <p>ID: {{ id }}</p> -->
+        <!-- <p>PLACE: {{ place.id }}</p> -->
+        <div>
+          <!-- <select v-model="sort" class="dropdown">
                         <option actve value="type">Arkiv: Alla</option>
                         <option value="year">Sort by: YEAR</option>
                     </select> -->
-                </div>
-                <!-- Sort by TYPE table-->
-                 <table class="content-table" v-if="sort === 'type' && documents.length > 0">
-                        <td>Documents: </td>
-                        <div v-for="(doc, index) in documents" :key="index" class="image-placeholder plan-placeholder">
-                        <div class="image-square">
-                            <router-link :to="`/detail/image/${doc.Nr}`">
-                                <div class="meta-data-overlay">
-                                    <div class="meta-data-overlay-text">{{ doc.Document }}</div>
-                                </div>
-                                <img :src="documentIcon" :alt="doc.title" class="image-square-plan" />
-                            </router-link>
-                        </div>
-                        </div>
-                </table>
-
-              
-            </div>
         </div>
+        <!-- Sort by TYPE table-->
+        <table class="content-table" v-if="sort === 'type' && documents.length > 0">
+          <td>Documents: </td>
+          <div v-for="(doc, index) in documents" :key="index" class="image-placeholder plan-placeholder">
+            <div class="image-square">
+              <router-link :to="`/detail/image/${doc.Nr}`">
+                <div class="meta-data-overlay">
+                  <div class="meta-data-overlay-text">{{ doc.Document }}</div>
+                </div>
+                <img :src="documentIcon" :alt="doc.title" class="image-square-plan" />
+              </router-link>
+            </div>
+          </div>
+        </table>
+
+
+      </div>
     </div>
+  </div>
+  
+  <MapComponent />
+
 </template>
 
     
 <style scoped>
 .image-square:hover img {
-    opacity: 0.3;
+  opacity: 0.3;
 }
 
 .meta-data-overlay-text {
-    bottom: 50px !important;
-    font-size: 20px;
-    color: white;
-}
-.main-container{
-    background-color:rgb(114,135,138) !important;
-    color:white;
+  bottom: 50px !important;
+  font-size: 20px;
+  color: white;
 }
 
-.content-table td{
-    color:white;
+.main-container {
+  background-color: rgba(114, 135, 138, 0.5) !important;
+  backdrop-filter: blur(10px) saturate(50%) brightness(100%);
+  color: white;
+}
+
+.content-table td {
+  color: white;
+}
+
+/* hides the zoom controls for the background map*/
+#app .ol-zoom-in {
+    display: none !important;
+}
+
+#app .ol-zoom-out {
+    display: none !important;
 }
 </style>
     
