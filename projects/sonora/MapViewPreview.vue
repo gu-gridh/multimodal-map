9bafb519c1475ad167acca556c3660acb30bb944
TristanBridge <tbridge360@gmail.com>
2024-02-14 14:39:44 +0100

    change loc if there's multiple organs per place

--- projects/sonora/MapViewPreview.vue
+++ projects/sonora/MapViewPreview.vue
@@ -19,19 +19,11 @@ const computedRoute = computed(() => `/place/${currentOrganNumber.value}`); //ro
 const { selectedBuilderId, builderLayerVisible, placeClicked } = storeToRefs(sonoraStore());
 const builderData = ref(null);
 const lastInteraction = ref('none'); //'none', 'place', or 'builder'
+const responseData = ref<null | Record<string, any>>(null);
 
 let currentOrganNumber = ref(0); //organ number corresponding to page in openseadragonviewer
 let text = ref(false)
 let place = ref()
-
-const placeInfo = ref({
-  Ort: '',
-  Byggnadens_namn: '',
-  Stift: '',
-  Kontrakt: '',
-  Kommun: '',
-  Loc: '',
-});
 
 //for filtering out the date from the Builder Name and putting it on a new line
 const builderNameAndDate = computed(() => {
@@ -49,8 +41,10 @@ const builderNameAndDate = computed(() => {
 });
 
 const handlePageChange = (newPage) => {
-    currentOrganNumber.value = organNumbers.value[newPage];
-    console.log(currentOrganNumber.value);
+  currentOrganNumber.value = organNumbers.value[newPage];
+  if (responseData.value) {
+    responseData.value.Loc = responseData.value[`loc${newPage}`] || ''; // update loc based on page
+  }
 };
 
 //when a place is selected, fetch image and info
@@ -64,15 +58,11 @@ watchEffect(async () => {
       const response = await fetch(`https://orgeldatabas.gu.se/webgoart/goart/place.php?id=${placeId}&lang=sv`);
       if (response.ok) {
         const data = await response.json();
-        placeInfo.value = {
-          Ort: data.Ort,
-          Byggnadens_namn: data.Byggnadens_namn,
-          Stift: data.Stift,
-          Kontrakt: data.Kontrakt,
-          Kommun: data.Kommun,
-          Loc: data.loc1,
-        };
+        responseData.value = data;
 
+        if (responseData.value) {
+          responseData.value.Loc = data.loc1 || '';
+        }
         // Initialize the organNumbers and imageUrls arrays
         organNumbers.value = {};
         imageUrls.value = [];
@@ -96,7 +86,7 @@ watchEffect(async () => {
       } else {
         imageUrls.value = [placeholderImage];
         organNumbers.value = {};
-        placeInfo.value = { Ort: '', Byggnadens_namn: '' };
+        responseData.value = {};
       }
     } catch (error) {
       console.error('Error fetching place info:', error);
@@ -105,8 +95,7 @@ watchEffect(async () => {
   } else {
     imageUrls.value = [];
     organNumbers.value = {};
-    // Reset placeInfo when no feature is selected
-    placeInfo.value = { Ort: '', Byggnadens_namn: '' };
+    responseData.value = {};
   }
 });
 
@@ -156,39 +145,31 @@ watch(() => placeClicked.value, (newValue) => {
           <OpenSeadragon :src="imageUrls" :key="imageUrls.join(',')" @page-changed="handlePageChange" class="flex-1" />
         </div>
         <div class="placecard-bottom">
-        <div class="placecard-text">
-          <div class="placecard-title theme-color-text">{{ placeInfo.Byggnadens_namn }}</div>
-          <div class="placecard-subtitle theme-color-text">{{ placeInfo.Ort }}</div>
+        <div class="placecard-text" v-if="responseData">
+          <div class="placecard-title theme-color-text">{{ responseData.Byggnadens_namn }}</div>
+          <div class="placecard-subtitle theme-color-text">{{ responseData.Ort }}</div>
         </div>
-        <div class="placecard-content">
+        <div class="placecard-content" v-if="responseData">
           <div class="placecard-metadata-content">
-            <div class="metadata-item" v-if="placeInfo.builder1">
-              <div class="label">Builder:</div>
-              <div class="tag theme-color-text">{{ placeInfo.builder1 }}</div>
+            <div class="metadata-item" v-if="responseData.Loc">
+              <div class="label">Location:</div>
+              <div class="tag theme-color-text">{{ responseData.Loc }}</div>
             </div>
-            <div class="metadata-item" v-if="placeInfo.year1">
-              <div class="label">Year:</div>
-              <div class="tag theme-color-text">{{ placeInfo.year1 }}</div>
-            </div>
-            <div class="metadata-item" v-if="placeInfo.loc1">
-              <div class="label">Loc:</div>
-              <div class="tag theme-color-text">{{ placeInfo.loc1 }}</div>
-            </div>
-            <div class="metadata-item" v-if="placeInfo.Stift">
+            <div class="metadata-item" v-if="responseData.Stift">
               <div class="label">Stift:</div>
-              <div class="tag theme-color-text">{{ placeInfo.Stift }}</div>
+              <div class="tag theme-color-text">{{ responseData.Stift }}</div>
             </div>
-            <div class="metadata-item" v-if="placeInfo.Kontrakt">
+            <div class="metadata-item" v-if="responseData.Kontrakt">
               <div class="label">Kontrakt:</div>
-              <div class="tag theme-color-text">{{ placeInfo.Kontrakt }}</div>
+              <div class="tag theme-color-text">{{ responseData.Kontrakt }}</div>
             </div>
-            <div class="metadata-item" v-if="placeInfo.Kommun">
+            <div class="metadata-item" v-if="responseData.Kommun">
               <div class="label">Kommun:</div>
-              <div class="tag theme-color-text">{{ placeInfo.Kommun }}</div>
+              <div class="tag theme-color-text">{{ responseData.Kommun }}</div>
             </div>
-            <div class="metadata-item" v-if="placeInfo.Pastorat">
+            <div class="metadata-item" v-if="responseData.Pastorat">
               <div class="label">Pastorat:</div>
-              <div class="tag theme-color-text">{{ placeInfo.Pastorat }}</div>
+              <div class="tag theme-color-text">{{ responseData.Pastorat }}</div>
             </div>
             
           </div>
