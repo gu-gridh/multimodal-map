18fd31174a6636bb01986d2899281e812450f8d8
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2024-04-30 12:45:01 +0200

    Update Footer.vue

--- projects/sonora/Footer.vue
+++ projects/sonora/Footer.vue
@@ -2,22 +2,28 @@
   <footer id="footer">
     <div id="developer-info">
       <div class="links-stack border-style">
-        <a class="site-link link" href="https://github.com/gu-gridh/multimodal-map/tree/sonora" target="_blank">GitHub repository and development</a>
-        <!-- <a class="site-link link" href="https://github.com/gu-gridh/etruscantombs#database-and-api-documentation" target="_blank">Database and API documentation</a>  -->
+        <a class="site-link link" href="https://www.organacademy.se/svenskt-digitalt-orgelarkiv" target="_blank">The SONORA research project</a>
+          <a class="site-link link" href="https://github.com/gu-gridh/multimodal-map/tree/sonora" target="_blank">GitHub
+            repository and development</a>
+      
       </div>
       <div class="links-stack">
-      <!-- <a class="site-link link" href="https://github.com/gu-gridh/etruscantombs#datasets" target="_blank">Dataset documentation</a> -->
-      <!-- <a href="#" class="download-link link" @click="downloadData">Download the structured data</a> -->
-    </div>
+        <a class="download-link" href="" target="_blank">
+          Texts and images: CC BY 4.0</a>
+        <a href="#" class="download-link link" @click="downloadData">Download the structured data</a>
+      </div>
     </div>
     <div class="partners">
-    <a href="https://dh.gu.se/">
-      <div class="link" id="PartnerLogo">+ SONORA</div>
-    </a>
-    <a href="https://dh.gu.se/">
-      <div class="link" id="CDHLogo">#GRIDH</div>
-    </a>
-  </div>
+      <a href="https://dh.gu.se/">
+        <div class="link GRIDHLogo" id="">GU / GRIDH</div>
+      </a>
+
+      <a href="https://www.organacademy.se/svenskt-digitalt-orgelarkiv">
+        <div class="link PartnerLogo" id="">SONORA</div>
+      </a>
+
+
+    </div>
   </footer>
 </template>
 
@@ -61,7 +67,7 @@ const triggerDownload = (data: any[], filename: string) => {
 
 <style>
 #footer {
-  display:block;
+  display: block;
   background-color: #222;
   color: white;
   line-height: 1;
@@ -70,7 +76,7 @@ const triggerDownload = (data: any[], filename: string) => {
   font-size: 12px;
   position: absolute;
   z-index: 3000;
-  bottom: -2px;;
+  bottom: -2px;
   font-family: "Barlow Condensed", sans-serif;
 }
 
@@ -78,50 +84,50 @@ a {
   font-weight: bold;
 }
 
-.partners{
-float: right;
-position:fixed;
-bottom:0px;
-right:0px;
-height:80px;
-width:600px;
+.partners {
+  float: right;
+  position: fixed;
+  bottom: 0px;
+  right: 0px;
+  height: 80px;
+  width:600px;
 }
 
-#CDHLogo {
+.GRIDHLogo {
   float: right;
-  margin-top: 0px;
-  margin-right: 10px;
+  margin-top: 30px;
   width: auto;
   height: auto;
   text-align: left;
   font-weight: 100;
   font-style: normal;
-  font-size: 45px;
-  line-height: 0.4;
-  padding: 30px 0px 10px 0px;
+  font-size: 30px;
+  line-height: 0.8;
+  padding: 0px 60px 5px 30px;
   vertical-align: middle;
-  color:white;
 }
 
-#PartnerLogo {
+
+.PartnerLogo {
   float: right;
-  margin-top: 0px;
-  margin-right: 40px;
+  margin-top: 30px;
   width: auto;
   height: auto;
   text-align: left;
   font-weight: 100;
   font-style: normal;
-  font-size: 45px;
-  line-height: 0.4;
-  padding: 30px 0px 10px 0px;
+  font-size: 30px;
+  line-height: 0.8;
+  padding: 0px 30px 5px 30px;
   vertical-align: middle;
-  color:white;
+  border-width: 0px 0.5px 0px 0px;
+  border-color: white;
+  border-style: solid;
 }
 
 #developer-info {
-  display: flex; 
-  align-items: flex-end; 
+  display: flex;
+  align-items: flex-end;
   margin-left: 50px;
   margin-top: 15px;
   font-size: 1.5em;
@@ -130,54 +136,53 @@ width:600px;
 }
 
 .border-style {
-  /* border-width: 0 0.5px 0 0px;
+  border-width: 0 0.5px 0 0px;
   border-color: white;
-  border-style: dashed; */
-  padding-left: 10px; 
-  padding-right: 20px; 
+  border-style: dashed;
+  padding-left: 10px;
+  padding-right: 20px;
 
 }
 
-.link{
-color:white;
+.link {
+  color: white;
 }
 
-.link:visited{
-color:white;
+.link:visited {
+  color: white;
 }
 
-.link:hover{
-opacity:0.8;
+.link:hover {
+  opacity: 0.8;
 }
 
-.download-link{
-  background:url("@/assets/interface/downloadbuttonwhite.png");
-  background-size:18px;
-  background-position:0px 50%;
-  background-repeat:no-repeat;
-  padding-left:25px;
-  margin-left:0px;
+.download-link {
+  background: url("@/assets/interface/downloadbuttonwhite.png");
+  background-size: 18px;
+  background-position: 0px 50%;
+  background-repeat: no-repeat;
+  padding-left: 25px;
+  margin-left: 0px;
 }
 
-.site-link{
-  background:url("@/assets/interface/linkbuttonwhite.png");
-  background-size:18px;
-  background-position:0px 50%;
-  background-repeat:no-repeat;
-  padding-left:25px;
+.site-link {
+  background: url("@/assets/interface/linkbuttonwhite.png");
+  background-size: 18px;
+  background-position: 0px 50%;
+  background-repeat: no-repeat;
+  padding-left: 25px;
 
 }
 
 .links-stack {
-  margin-right: 20px; 
+  margin-right: 20px;
 }
 
 .links-stack a {
   display: block;
 }
 
-#developer-info a:link{
-  font-weight:200;
+#developer-info a:link {
+  font-weight: 200;
 }
-
 </style>
