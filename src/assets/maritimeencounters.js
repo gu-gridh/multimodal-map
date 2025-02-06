ca58f3fce28a9865752e2938b8b49c2207d0c47c
Tristan Bridge <tbridge360@gmail.com>
2025-02-06 10:25:11 +0100

    fix ts for ME

--- src/assets/maritimeencounters.js
+++ src/assets/maritimeencounters.js
@@ -1,46 +1,39 @@
 import axios from "axios";
-import type { ListResponse, MapResponse } from "@/types/diana";
 
 export const MARITIMEENCOUNTERS_BASE = "https://maritime-encounters.dh.gu.se/api/";
 
 export class MaritimeEncountersClient {
-  project: string;
-
-  constructor(project: string) {
+  constructor(project) {
     this.project = project;
   }
 
-  async list<T>(type: string, params?: object): Promise<ListResponse<T>> {
-    const response = await axios.get<ListResponse<T>>(
+  async list(type, params) {
+    const response = await axios.get(
       MARITIMEENCOUNTERS_BASE + `${this.project}/${type}/`,
       { params }
     );
     return response.data;
   }
 
-  async listAll<T>(
-    type: string,
-    params?: Record<any, any>,
-    limit = 500
-  ): Promise<T[]> {
+  async listAll(type, params, limit = 500) {
     params = { ...params, limit };
-    const response = await axios.get<ListResponse<T>>(
+    const response = await axios.get(
       MARITIMEENCOUNTERS_BASE + `${this.project}/${type}/`,
       { params }
     );
     return response.data.results;
   }
 
-  async map<T>(type: string, params?: object): Promise<MapResponse<T>> {
-    const response = await axios.get<MapResponse<T>>(
+  async map(type, params) {
+    const response = await axios.get(
       MARITIMEENCOUNTERS_BASE + `${this.project}/${type}/`,
       { params }
     );
     return response.data;
   }
 
-  async get<T>(type: string, id: string | number, params?: object): Promise<T> {
-    const response = await axios.get<T>(
+  async get(type, id, params) {
+    const response = await axios.get(
       MARITIMEENCOUNTERS_BASE + `${this.project}/${type}/${id}/`,
       { params }
     );
