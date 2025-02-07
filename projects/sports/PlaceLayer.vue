5d9568d3e1c3ef891b570858dfed08c60ee076f4
kikkipedia <kristin.akerlund@lir.gu.se>
2025-02-07 12:47:17 +0100

    Added a filter to the map (sustainable travel to sports)

--- projects/sports/PlaceLayer.vue
+++ projects/sports/PlaceLayer.vue
@@ -10,6 +10,7 @@ import { onMounted, ref } from "vue";
 
 const map = ref(null);
 
+
 onMounted(async () => {
   map.value = L.map("map").setView([59.8586, 17.6389], 10); //uppsala
 
@@ -27,26 +28,58 @@ onMounted(async () => {
 
   const allLatLngPolygons = []; 
 
-  geojsonData.features.forEach((feature) => {
-    const geometry = feature.geometry;
+  geojsonData.features.forEach((feature) => { //only show sustainable data // this can be chaneged to car/bike/walking or ALL but that will put them on top of each other
+    if (feature.properties.mode == "sustainable"){ 
+      const geometry = feature.geometry;
 
-    if (geometry.type === "MultiPolygon") {
-      geometry.coordinates.forEach((polygon) => {
-        const latLngPolygon = polygon[0].map((coord) => {
-          const [lon, lat] = proj4(utm33n, "WGS84", coord); //convert UTM to WGS84
-          return [lat, lon]; //flip latitude and longitude
+      if (geometry.type === "MultiPolygon") {
+        geometry.coordinates.forEach((polygon) => {
+          const latLngPolygon = polygon[0].map((coord) => {
+            const [lon, lat] = proj4(utm33n, "WGS84", coord); //convert UTM to WGS84
+            return [lat, lon]; //flip latitude and longitude
+          });
+
+          allLatLngPolygons.push(latLngPolygon);
+
+          //set polygon color depending on value of dd_15_min_total // this can be changed to other values
+          const dd_15 = feature.properties.dd_15_min_;
+          if (dd_15 === null || dd_15 === 0) {
+            L.polygon(latLngPolygon, {
+              color: "whitee",
+              fillColor: "white",
+              fillOpacity: 0.4,
+              weight: 1,
+            }).addTo(map.value);
+          }
+          else if (dd_15 > 0 && dd_15 < 6) {
+            L.polygon(latLngPolygon, {
+              color: "yellow",
+              fillColor: "yellow",
+              fillOpacity: 0.4,
+              weight: 1,
+            }).addTo(map.value);
+          }
+          else if (dd_15 >= 6 && dd_15 < 10) {
+            L.polygon(latLngPolygon, {
+              color: "orange",
+              fillColor: "orange",
+              fillOpacity: 0.4,
+              weight: 1,
+            }).addTo(map.value);
+          }
+          
+          else {
+            L.polygon(latLngPolygon, {
+              color: "red",
+              fillColor: "red",
+              fillOpacity: 0.4,
+              weight: 1,
+            }).addTo(map.value);
+          }
         });
-
-        allLatLngPolygons.push(latLngPolygon);
-
-        L.polygon(latLngPolygon, {
-          color: "green",
-          fillColor: "lime",
-          fillOpacity: 0.4,
-          weight: 1,
-        }).addTo(map.value);
-      });
+      }
     }
+
   });
 
   //fit the map bounds
