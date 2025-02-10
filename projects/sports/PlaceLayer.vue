62e1fb7df3ec06b0ffb5e56bbe9776c0dc2475c5
kikkipedia <kristin.akerlund@lir.gu.se>
2025-02-10 15:42:22 +0100

    Polygon colors depending on activities

--- projects/sports/PlaceLayer.vue
+++ projects/sports/PlaceLayer.vue
@@ -13,6 +13,8 @@ const map = ref(null);
 
 const sportsStore = useSportsStore();
 
+const geojsonData = ref(null);
+
 //this is for all types of sports
 const travelTimes = ref({
   15: "15_total", 
@@ -27,15 +29,25 @@ const mapStyles = ref({
 })
 
 onMounted(async () => {
+  initMap();
+});
+
+//watch for store updates
+watch(() => sportsStore.travelTime, () => {
+  console.log("Travel time changed to", sportsStore.travelTime);
+
+});
+
+const initMap = async () => {
   map.value = L.map("map").setView([59.8586, 17.6389], 10); //uppsala
 
   L.tileLayer(mapStyles.value.OSM, { 
     attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map.value);
-  
+
 
   const response = await fetch("./uppsala_t1.geojson");
-  const geojsonData = await response.json();
+  geojsonData.value = await response.json();
 
   console.log("GeoJSON Loaded:", geojsonData);
 
@@ -44,7 +56,7 @@ onMounted(async () => {
   const allLatLngPolygons = []; 
 
 
-  geojsonData.features.forEach((feature) => { 
+  geojsonData.value.features.forEach((feature) => { 
     if (feature.properties.mode == sportsStore.travelMode){ // this can be chaneged to car/bike/walking or ALL but that will put them on top of each other
       const geometry = feature.geometry;
 
@@ -59,53 +71,67 @@ onMounted(async () => {
 
           //set polygon color depending on value of number of unique activities
           const time = feature.properties[travelTimes.value[sportsStore.travelTime]]; //set the time value to be displayed
-          if (time === null || time === 0) { // no activites
+
             L.polygon(latLngPolygon, { 
-              color: "blue",
-              fillColor: "blue",
-              fillOpacity: 0.6,
+              color: "white",
+              fillColor: setColor(time),
+              fillOpacity: 0.4,
               weight: 1,
+              dashArray: "2, 2"
             }).addTo(map.value);
-          }
-          else if (time > 0 && time < 6) { // low activites 1-6
-            L.polygon(latLngPolygon, {
-              color: "yellow",
-              fillColor: "yellow",
-              fillOpacity: 0.6,
-              weight: 1,
-            }).addTo(map.value);
-          }
-          else if (time >= 6 && time < 10) { // medium activites 6-10
-            L.polygon(latLngPolygon, {
-              color: "orange",
-              fillColor: "orange",
-              fillOpacity: 0.6,
-              weight: 1,
-            }).addTo(map.value);
-          }
           
-          else {
-            L.polygon(latLngPolygon, { // high activites 10+
-              color: "red",
-              fillColor: "red",
-              fillOpacity: 0.6,
-              weight: 1,
-            }).addTo(map.value);
-          }
+
         });
       }
-    }
 
-  });
 
-  //fit the map bounds
-  const bounds = L.polygon(allLatLngPolygons.flat()).getBounds();
-  map.value.fitBounds(bounds);
+  }
+
 });
 
-//watch for store updates
-watch(() => sportsStore, () => {
-  //redraw map
-});
+        //add legend
+        // const legend = L.control({ position: "bottomright" });
+        // legend.onAdd = function (map) {
+        //   const div = L.DomUtil.create("div", "info legend");
+        //   const grades = [0, 6, 10];
+        //   const labels = [];
+        //   for (let i = 0; i < grades.length; i++) {
+        //     div.innerHTML += 
+        //       '<i style="background:' + setColor(grades[i] + 1) + '"></i> ' +
+        //       grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+")
+            
+        //   }
+        //   return div;
+        // };
+        // legend.addTo(map.value);
+
+//fit the map bounds
+const bounds = L.polygon(allLatLngPolygons.flat()).getBounds();
+map.value.fitBounds(bounds);
+};
+
+const setColor = (time) => {
+  return time === null || time === 0 ? "blue" : time > 0 && time < 6 ? "yellow" : time >= 6 && time < 10 ? "orange" : "red";
+}
 
 </script>
+
+<style scoped>
+.legend {
+    line-height: 18px;
+    color: #555;
+    padding-bottom: 20px ;
+    background-color: white !important;
+}
+.legend i {
+    width: 18px;
+    height: 18px;
+    float: left;
+    margin-right: 8px;
+    opacity: 0.7;
+}
+
+#map.leaflet-container {
+    height: calc(100vh - 80px) !important;
+}
+</style>
