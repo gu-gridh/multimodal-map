326e81b4c8184eb401b25615ca730d182e5f745d
kikkipedia <kristin.akerlund@lir.gu.se>
2025-02-20 09:44:33 +0100

    Show correct layer when 'all' is selected

--- projects/rwanda/input/DropdownComponent.vue
+++ projects/rwanda/input/DropdownComponent.vue
@@ -13,6 +13,9 @@
 </template>
 
 <script setup>
+import { storeToRefs } from "pinia";
+import { rwandaStore } from "../settings/rwandaStore";
+
 
 const props = defineProps({
   modelValue: {
@@ -35,6 +38,8 @@ const props = defineProps({
   },
 });
 
+const { showAdvancedLayer, allLayer } = storeToRefs(rwandaStore());
+
 const emit = defineEmits(["update:modelValue", "click"]);
 
 function toggle(key) {
@@ -46,15 +51,22 @@ function dropdownToggle(event) {
   
   if (selectElement) {
     const selectedKey = selectElement.value;
+    if (selectedKey === '') {;
+      showAdvancedLayer.value = false;
+      allLayer.value = true;
+      return;
+    }
     handleToggle(selectedKey);
   }
 }
 
 function handleToggle(key) {
   if (key === 'all') {
+   
     emit("update:modelValue", ['all']); // Set modelValue to ['all']
 
     emit('click', 'all'); // Emit the special 'all' value
+    
     return;
   }
   
@@ -62,6 +74,7 @@ function handleToggle(key) {
   if (newValue.includes(key)) {
     newValue.splice(newValue.indexOf(key), 1);
   } else if (props.limit === 1) {
+    
     newValue = [key];
   } else {
     newValue.push(key);
