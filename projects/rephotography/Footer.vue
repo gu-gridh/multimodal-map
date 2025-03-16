74f7481a2921e39abb6d53a162f54f3d0768cf57
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2025-03-16 16:42:41 +0100

    Footer fix

--- projects/rephotography/Footer.vue
+++ projects/rephotography/Footer.vue
@@ -1,14 +1,14 @@
 <template>
   <footer id="footer">
     <div id="developer-info">
-      <div class="links-stack border-style">
+      <div class="links-stack">
         <a class="site-link link" href="https://github.com/gu-gridh/etruscantombs#etruscan-tombs" target="_blank">GitHub repository and development</a>
         <a class="site-link link" href="https://github.com/gu-gridh/etruscantombs#database-and-api-documentation" target="_blank">Database and API documentation</a>
       </div>
-      <div class="links-stack">
+      <!-- <div class="links-stack border-style">
       <a class="site-link link" href="https://github.com/gu-gridh/etruscantombs#datasets" target="_blank">Dataset documentation</a>
       <a href="#" class="download-link link" @click="downloadData">Download the structured data</a>
-    </div>
+    </div> -->
     </div>
     <div class="partners">
       <a href="https://dh.gu.se/">
@@ -130,11 +130,11 @@ a {
 }
 
 .border-style {
-  border-width: 0 0.5px 0 0px;
+  border-width: 0 0 0 0.5px;
   border-color: white;
   border-style: dashed;
-  padding-left: 10px; 
-  padding-right: 20px; 
+  padding-left: 20px; 
+  padding-right: 10px; 
 
 }
 
