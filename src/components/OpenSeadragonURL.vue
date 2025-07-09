d8dc32dd7fed62de23c7d7555d64f7911c533557
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2025-07-09 13:31:25 +0200

    common settings for digicure image viewers (and jubileum)

--- src/components/OpenSeadragonURL.vue
+++ src/components/OpenSeadragonURL.vue
@@ -21,20 +21,21 @@ onMounted(() => {
     /** @see https://openseadragon.github.io/docs/OpenSeadragon.html#.Options */
      element: viewerEl.value,
      preserveViewport: true,
-     sequenceMode: false,
-    immediateRender: false,
-    visibilityRatio: 1.0,
-    minZoomImageRatio: 1.0,
-    homeFillsViewer: false,
-    showZoomControl: true,
-    showHomeControl: false,
-    showFullPageControl: true,
-    showNavigator:  true,
-    //navigatorId:   "navigatorDiv",
-    navigatorAutoFade:  true,
-    fullPageButton: "full-page",
-    zoomInButton: "zoom-in",
-    zoomOutButton: "zoom-out",
+   immediateRender: false,
+          visibilityRatio: 0.6,
+          minZoomImageRatio: 0.5,
+          homeFillsViewer: true,
+          showZoomControl: true,
+          showHomeControl: true,
+          //defaultZoomLevel:0.6,
+          showFullPageControl: true,
+          showNavigator: true,
+          navigatorAutoFade: true,
+          //navigatorId:   "navigator-div",
+          fullPageButton: "full-page",
+          zoomInButton: "zoom-in",
+          zoomOutButton: "zoom-out",
+          homeButton: "home",
     prefixUrl: "/openseadragon/",
     // If a proper tileSource is not given, use src prop.
     tileSources: props.src,
@@ -52,17 +53,23 @@ onMounted(() => {
 
 <template>
   <div ref="viewerEl" class="osd">
-    <div id="ToolbarVertical">
-      <a id="full-page" href="#full-page">
-        <div id="FullPage" class="NavButton"></div>
-      </a>
-      <a id="zoom-in" href="#zoom-in">
-        <div id="ZoomIn" class="NavButton"></div>
-      </a>
-      <a id="zoom-out" href="#zoom-out">
-        <div id="ZoomOut" class="NavButton"></div>
+    <div class="interface-area">
+      <div class="toolbar-bottom-mini">
 
-      </a>
+        <a id="home" href="#home">
+          <div id="Home" class="NavButton" title="Fit image to view"></div>
+        </a>
+        <a id="zoom-in" href="#zoom-in">
+          <div id="ZoomIn" class="NavButton"></div>
+        </a>
+        <a id="zoom-out" href="#zoom-out">
+          <div id="ZoomOut" class="NavButton"></div>
+        </a>
+        <a id="full-page" class="full-screen-option" href="#full-page">
+          <div id="FullPage" class="NavButton"></div>
+        </a>
+
+      </div>
     </div>
   </div>
 </template>
@@ -81,116 +88,11 @@ onMounted(() => {
 
 }
 
-#ToolbarHorizontal {
-  position: absolute;
-  display: flex;
-  bottom: 10px;
-  width: 100%;
-  z-index: 1000;
-}
-
-#CenterNav {
-  margin: auto;
-}
-
-#ToolbarHorizontal span {
-  background-color: rgba(35, 35, 35, 0.6);
-  color: white;
-  text-align: center;
-  padding: 5px 10px 5px 10px;
-  border-radius: 8px;
-  font-size: 18px;
-  font-weight: bold;
-  vertical-align: 13px;
-  margin-left: 10px;
-  margin-right: 10px;
-}
 
 
-#ToolbarVertical {
-  position: absolute;
-  top: 20px;
-  width: 60px;
-  margin-left: 15px;
-  z-index: 1000;
-  cursor:pointer;
-}
 
-#FullPage {
-  background: url(@/assets/openseadragon/expand.svg);
-  background-size: 100%;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-color: rgba(35, 35, 35, 0.9);
-  border-radius: 50%;
-  width: 35px;
-  height: 35px;
-  border-radius: 50%;
-  overflow: hidden;
-  cursor:pointer;
-}
 
-#Prev {
-  background: url(@/assets/openseadragon/prev.png);
-  background-size: 35px 35px;
-  background-color: rgba(35, 35, 35, 0.9);
-  background-repeat: no-repeat;
-  background-position: center;
-  display: inline-block;
-  position: relative;
-}
 
-#Next {
-  background: url(@/assets/openseadragon/next.png);
-  background-size: 35px 35px;
-  background-color: rgba(35, 35, 35, 0.9);
-  background-repeat: no-repeat;
-  background-position: center;
-  display: inline-block;
-  position: relative;
-}
+ /*      DigiCure Look */
 
-#ZoomIn {
-  background: url(@/assets/openseadragon/plus.svg);
-  background-size: 100%;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-color: rgba(35, 35, 35, 0.9);
-  border-radius: 50%;
-  width: 35px;
-  height: 35px;
-  margin-top: 10px;
-  cursor:pointer;
-  overflow: hidden;
-}
-
-#ZoomOut {
-  background: url(@/assets/openseadragon/minus.svg);
-  background-size: 100%;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-color: rgba(35, 35, 35, 0.9);
-  border-radius: 50%;
-  width: 35px;
-  height: 35px;
-  cursor:pointer;
-  overflow: hidden;
-}
-.NavButton {
-  width: 35px;
-  height: 35px;
-  border-radius: 50%;
-  color: white;
-  opacity: 0.7;
-  margin-bottom: 3px;
-  cursor:pointer;
-}
-.NavButton:hover {
-  opacity: 1;
-  cursor:pointer;
-}
-
-*:focus {
-        outline:none!important;
-      }
 </style>
