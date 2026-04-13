b4e7b2f44d3b0ea9bb5320e643fa1598de9535a7
Tristan Bridge <tbridge360@gmail.com>
2026-04-13 12:54:47 +0200

    better fitting carousel

--- projects/sonora/About.vue
+++ projects/sonora/About.vue
@@ -44,11 +44,13 @@
         </div>
 
         <div class="carousel-container" v-if="displayAbout">
-          <Carousel v-bind="settings" :breakpoints="breakpoints" class="custom-carousel">
+          <Carousel ref="aboutCarousel" v-model="currentSlide" v-bind="settings" :breakpoints="breakpoints"
+            class="custom-carousel">
             <Slide v-for="slide in slides" :key="slide.id">
               <div class="carousel__item">
                 <div class="carousel__images">
                   <img v-for="(image, index) in slide.images" :key="index" :src="image" alt="slide image"
+                    @load="updateCarouselHeight"
                     :style="image === '/sonora_archive/Moberg-2.jpg' ? { maxHeight: '300px', margin: '0 auto' } : { maxWidth: '300px', maxHeight: '300px', margin: '0 auto' }" />
                 </div>
                 <div class="text-content">
@@ -88,6 +90,7 @@ export default {
   data() {
     return {
       displayAbout: this.visibleAbout,
+      currentSlide: 0,
       //carousel settings
       settings: {
         itemsToShow: 1,
@@ -164,10 +167,14 @@ export default {
     };
   },
   watch: {
+    currentSlide() {
+      this.updateCarouselHeight();
+    },
     visibleAbout(newVal) {
       if (newVal) {
         //if visibleAbout is true, immediately show the container
         this.displayAbout = true;
+        this.updateCarouselHeight();
       } else {
         //if visibleAbout is false, wait for the transition to finish before hiding
         setTimeout(() => {
@@ -176,6 +183,13 @@ export default {
       }
     },
   },
+  mounted() {
+    this.updateCarouselHeight();
+    window.addEventListener('resize', this.updateCarouselHeight);
+  },
+  beforeUnmount() {
+    window.removeEventListener('resize', this.updateCarouselHeight);
+  },
   methods: {
     toggleLanguage() {
       if (i18n.global.locale === 'en') {
@@ -183,7 +197,23 @@ export default {
       } else {
         i18n.global.locale = 'en';
       }
+
+      this.updateCarouselHeight();
     },
+    updateCarouselHeight() {
+      this.$nextTick(() => {
+        const carouselElement = this.$refs.aboutCarousel?.$el;
+
+        if (!carouselElement) return;
+
+        const viewport = carouselElement.querySelector('.carousel__viewport');
+        const activeSlide = carouselElement.querySelector('.carousel__slide--active .carousel__item');
+
+        if (!viewport || !activeSlide) return;
+
+        viewport.style.height = `${activeSlide.offsetHeight}px`;
+      });
+    }
   },
 };
 </script>
@@ -270,6 +300,10 @@ ul {
   background: none;
 }
 
+.carousel-container .custom-carousel :deep(.carousel__track) {
+  align-items: flex-start;
+}
+
 .carousel-container h3 {
   font-size: 1.5em;
   margin-bottom: 2%;
@@ -320,6 +354,12 @@ ul {
   margin-top: 1em;
 }
 
+.carousel__text :deep(a) {
+  color: inherit;
+  font-weight: 400;
+  text-decoration: underline;
+}
+
 .carousel__item h3 {
   display: block;
   background: none;
@@ -331,6 +371,10 @@ ul {
 .custom-carousel .carousel__slide {
   align-items: center !important;
   justify-content: center !important;
+}
+
+.custom-carousel :deep(.carousel__slide) {
+  align-items: flex-start;
 }
 
 .text-content {
@@ -389,7 +433,8 @@ ul {
 
 .carousel-container .custom-carousel :deep(.carousel__prev),
 .carousel-container .custom-carousel :deep(.carousel__next) {
-  top: 40%;
+  top: 170px;
+  transform: none;
   width: 72px !important;
   height: 72px !important;
 }
